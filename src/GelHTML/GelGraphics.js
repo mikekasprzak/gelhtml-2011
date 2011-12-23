8f68a55c0a21ab9073f0850e342c68a849113049
mikekasprzak <mikekasprzak@gmail.com>
2011-12-23 12:16:58 +0000

    Imported new code added during Ludum Dare 22, and revised template. Improved compatibility across both desktop and mobile browsers. You could make a shippable game with this now.

--- src/GelHTML/GelGraphics.js
+++ src/GelHTML/GelGraphics.js
@@ -27,12 +27,13 @@ function gelCenterImage( img, x, y, scale_x, scale_y ) {
 
 // - -------------------------------------------------------------------------------------------------------------- - //
 function gelSetColor( r, g, b, a ) {
+//	Log( typeof a );
 	if ( typeof a === 'undefined' ) {
 		ctx.fillStyle = "rgb(" + r + "," + g + "," + b + ")";
 		ctx.strokeStyle = ctx.fillStyle;
 	}
 	else {
-		ctx.fillStyle = "rgba(" + r + "," + g + "," + b + "," + a + ")";
+		ctx.fillStyle = "rgba(" + r + "," + g + "," + b + "," + (a / 255) + ")";
 		ctx.strokeStyle = ctx.fillStyle;
 	}
 }
@@ -42,23 +43,115 @@ function gelSetColor( r, g, b, a ) {
 function gelDrawRectFill( x, y, w, h ) {
 	ctx.fillRect( x, y, w, h );
 }
-//	ctx.beginPath();
-//	ctx.rect( x, y, w, h );
-//	ctx.closePath();
-//	ctx.fill();	
-//}
 // - -------------------------------------------------------------------------------------------------------------- - //
 function gelDrawRect( x, y, w, h ) {
-	strokeRect( x, y, w, h );
+	ctx.strokeRect( x, y, w, h );
 }
-//	ctx.beginPath();
-//	ctx.rect( x, y, w, h );
-//	ctx.closePath();
-//	ctx.stroke();
-//}
 // - -------------------------------------------------------------------------------------------------------------- - //
 function gelClearRect( x, y, w, h ) {
-	clearRect( x, y, w, h );
+	ctx.clearRect( x, y, w, h );
+}
+// - -------------------------------------------------------------------------------------------------------------- - //
+
+// - -------------------------------------------------------------------------------------------------------------- - //
+function gelDrawTriangleShape( x1, y1, x2, y2, x3, y3 ) {
+	ctx.beginPath();
+	ctx.moveTo( x1, y1 );
+
+	ctx.lineTo( x2, y2 );
+	ctx.lineTo( x3, y3 );
+	ctx.lineTo( x1, y1 );
+
+	ctx.closePath();
+	ctx.stroke();
+}
+// - -------------------------------------------------------------------------------------------------------------- - //
+function gelDrawTriangleShapeFill( x1, y1, x2, y2, x3, y3 ) {
+	ctx.beginPath();
+	ctx.moveTo( x1, y1 );
+
+	ctx.lineTo( x2, y2 );
+	ctx.lineTo( x3, y3 );
+	ctx.lineTo( x1, y1 );
+
+	ctx.closePath();
+	ctx.fill();
+}
+// - -------------------------------------------------------------------------------------------------------------- - //
+function gelDrawQuadShape( x1, y1, x2, y2, x3, y3, x4, y4 ) {
+	ctx.beginPath();
+	ctx.moveTo( x1, y1 );
+
+	ctx.lineTo( x2, y2 );
+	ctx.lineTo( x3, y3 );
+	ctx.lineTo( x4, y4 );
+	ctx.lineTo( x1, y1 );
+
+	ctx.closePath();
+	ctx.stroke();
+}
+// - -------------------------------------------------------------------------------------------------------------- - //
+function gelDrawQuadShapeFill( x1, y1, x2, y2, x3, y3, x4, y4 ) {
+	ctx.beginPath();
+	ctx.moveTo( x1, y1 );
+
+	ctx.lineTo( x2, y2 );
+	ctx.lineTo( x3, y3 );
+	ctx.lineTo( x4, y4 );
+	ctx.lineTo( x1, y1 );
+
+	ctx.closePath();
+	ctx.fill();
+}
+// - -------------------------------------------------------------------------------------------------------------- - //
+
+// - -------------------------------------------------------------------------------------------------------------- - //
+function gelDrawWedgeBottomRight( x1, y1, w, h ) {
+	var x2 = x1+w;
+	var y2 = y1+h;
+	gelDrawTriangleShape( x2,y1, x2,y2, x1,y2 );
+}
+// - -------------------------------------------------------------------------------------------------------------- - //
+function gelDrawWedgeBottomLeft( x1, y1, w, h ) {
+	var x2 = x1+w;
+	var y2 = y1+h;
+	gelDrawTriangleShape( x1,y1, x2,y2, x1,y2 );
+}
+// - -------------------------------------------------------------------------------------------------------------- - //
+function gelDrawWedgeTopRight( x1, y1, w, h ) {
+	var x2 = x1+w;
+	var y2 = y1+h;
+	gelDrawTriangleShape( x1,y1, x2,y1, x2,y2 );
+}
+// - -------------------------------------------------------------------------------------------------------------- - //
+function gelDrawWedgeTopLeft( x1, y1, w, h ) {
+	var x2 = x1+w;
+	var y2 = y1+h;
+	gelDrawTriangleShape( x1,y1, x2,y1, x1,y2 );
+}
+// - -------------------------------------------------------------------------------------------------------------- - //
+function gelDrawWedgeBottomRightFill( x1, y1, w, h ) {
+	var x2 = x1+w;
+	var y2 = y1+h;
+	gelDrawTriangleShapeFill( x2,y1, x2,y2, x1,y2 );
+}
+// - -------------------------------------------------------------------------------------------------------------- - //
+function gelDrawWedgeBottomLeftFill( x1, y1, w, h ) {
+	var x2 = x1+w;
+	var y2 = y1+h;
+	gelDrawTriangleShapeFill( x1,y1, x2,y2, x1,y2 );
+}
+// - -------------------------------------------------------------------------------------------------------------- - //
+function gelDrawWedgeTopRightFill( x1, y1, w, h ) {
+	var x2 = x1+w;
+	var y2 = y1+h;
+	gelDrawTriangleShapeFill( x1,y1, x2,y1, x2,y2 );
+}
+// - -------------------------------------------------------------------------------------------------------------- - //
+function gelDrawWedgeTopLeftFill( x1, y1, w, h ) {
+	var x2 = x1+w;
+	var y2 = y1+h;
+	gelDrawTriangleShapeFill( x1,y1, x2,y1, x1,y2 );
 }
 // - -------------------------------------------------------------------------------------------------------------- - //
 
